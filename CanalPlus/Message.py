9e8cfc93a85db36edc8d0b7c7140875328fce66b
Robin Elrharbi--Fleury <robin.elrharbi-fleury@phelma.grenoble-inp.fr>
2016-11-22 21:27:59 +0100

    save

--- CanalPlus/Message.py
+++ CanalPlus/Message.py
@@ -1,50 +1,47 @@
 # encoding: utf-8
 import time
 
-TIME_BEFORE_SENDING_AGAIN_ms = 150
+TIME_BEFORE_SENDING_AGAIN_ms = 150 #ms
 
 class Message(object):
-	"""docstring for Message"""
-	def __init__(self, type = 'undefined'):
-		super(Message, self).__init__()
-		self.type = type
-		self.size = 8
-		self.id = '1234' + 'time'
-		self.ack_number = -1
-		self.content = []
-		self.ack_status = False
-		self.time_since_last_try = 0
+  """docstring for Message"""
+  def __init__(self, type = 'data'):
+    super(Message, self).__init__()
+    self.type = type
+    self.id = '1234' + 'time'
+    self.ack_number = -1
+    self.ack_status = False
+    self.time_since_last_try = 0
+    self.content = []
 
-	def add_content(self, content):
-		self.content.append(content)
-		print ("Not implemented yet")
-		pass
+  def wrapping(self):
+    print ("Not implemented yet")
+    pass
 
-	def get_id(self):
-		return self.id
+  def time_since_last_try_not_short(self):
+    diff = time.gmtime() - self.time_since_last_try
+    diff = diff * 1000
+    return diff > TIME_BEFORE_SENDING_AGAIN_ms 
 
-	def set_ack_number(self, ack_number):
-		self.ack_number = ack_number
+  def has_been_received(self):
+    self.ack_status = True
 
-	def get_ack_number(self):
-		return self ack_number
+  def try_to_send(self):
+    self.time_since_last_try = time.gmtime()
+    print ("Not implemented yet")
 
-	def wrapping(self)
-		print ("Not implemented yet")
-		pass
+  def add_content(self, header, data):
+    self.content.append(header)
+    self.content.append(data)
 
-	def get_ack_status(self):
-		return self.ack_status
+  def set_ack_number(self, ack_number):
+    self.ack_number = ack_number
 
-	def has_been_received(self):
-		self.ack_status = True
+  def get_id(self):
+    return self.id
 
-	def time_since_last_try_not_short(self):
-		diff = time.gmtime() - self.time_since_last_try
-		diff = diff * 1000
-		return diff > TIME_BEFORE_SENDING_AGAIN_ms 
+  def get_ack_number(self):
+    return self ack_number
 
-	def try_to_send(self):
-		self.time_since_last_try = time.gmtime()
-		print ("Not implemented yet")
-
+  def get_ack_status(self):
+    return self.ack_status
--- CanalPlus/SendingThread.py
+++ CanalPlus/SendingThread.py
@@ -4,77 +4,78 @@ from Message import *
 import time
 
 class SendingThread(threading.Thread):
-    def __init__(self, connection, ip_address):
-        super(SendingThread, self).__init__()
-        self.target_ip_address = ip_address
-        self.connection = connection
-        self.target_buffer_full = False
-        self.sending_list = []
-        self.ack_array = []
-        self.ack_array_next_free_cell = []
+  def __init__(self, connection, ip_address):
+    super(SendingThread, self).__init__()
+    self.target_ip_address = ip_address
+    self.connection = connection
+    self.target_buffer_full = False
+    self.sending_list = []
+    self.ack_array = []
+    self.ack_array_next_free_cell = []
 
-    def run(self):
-        self.start_receiving_acks(self)
-        self.establish_connection(self)
-        while self.connection.status == "established":
-            if self.has_message_to_process() and self.target_buffer_not_full():
-                self.send_next_message()
-            else:
-                continue
-        self.handle_connection_ending()
+  def run(self):
+    self.start_receiving_acks(self)
+    self.establish_connection(self)
+    while self.connection.status == "established":
+      if self.has_message_to_process() and self.target_buffer_not_full():
+        self.send_next_message()
+      else:
+        continue
+    self.handle_connection_ending()
 
-    def add(self, Message message):
-        message.wrapping()
-        self.append_to_sending_list(message)
-        self.add_to_ack_array(message)
+  def add_data(self, format, data):
+    message.add_content(header, formated_data) ######## To do
+    message.wrapping()
+    self.append_to_sending_list(message)
+    self.add_to_ack_array(message)
+  
+  def send_next_message(self):
+    message = self.pop_next_message();
+    if not message.has_been_read():
+      if message.time_since_last_try_not_short():
+        message.try_to_send()
+      self.append_to_sending_list(message)
+    else:
+      self.remove_message(message)
+      message.delete()
+
+  def start_receiving_acks(self):
+    self.ack_receiving_thread = ReceivingAckThread(self.ack_queue)
+    self.ack_receiving_thread.start()
+
+  def establish_connection():
+    print ("Not implemented yet")
+    pass
+
+  def has_message_to_process():
+    return len(sending_list) > 0
+
+  def target_buffer_not_full():
+    return not self.target_buffer_full
+
+  def handle_connection_ending():
+    print ("Not implemented yet")
+    pass
     
-    def send_next_message(self):
-        message = self.pop_next_message();
-        if not message.has_been_read():
-            if message.time_since_last_try_not_short():
-                message.try_to_send()
-            self.append_to_sending_list(message)
-        else:
-            self.remove_message(message)
-            message.delete()
+  def add_to_ack_array(Message message):
+    indice = self.ack_array_next_free_cell.pop()
+    self.ack_array[indice] = False
+    message.set_ack_number(indice)
 
-    def start_receiving_acks(self):
-        self.ack_receiving_thread = ReceivingAckThread(self.ack_queue)
-        self.ack_receiving_thread.start()
+  def pop_next_message(self):
+    Message message = self.sending_list.pop()
+    int ack = message.get_ack_number()
+    if self.ack_array[ack]:
+      message.has_been_read()
+    return message
 
-    def establish_connection():
-        print ("Not implemented yet")
-        pass
+  def append_to_sending_list(Message message):
+    self.sending_list.append(message)
 
-    def has_message_to_process():
-        return len(sending_list) > 0
-
-    def target_buffer_not_full():
-        return not self.target_buffer_full
-
-    def handle_connection_ending():
-        print ("Not implemented yet")
-        pass
-        
-    def add_to_ack_array(Message message):
-        indice = self.ack_array_next_free_cell.pop()
-        self.ack_array[indice] = False
-        message.set_ack_number(indice)
-
-    def pop_next_message(self):
-        Message message = self.sending_list.pop()
-        int ack = message.get_ack_number()
-        if self.ack_array[ack]:
-            message.has_been_read()
-        return message
-
-    def append_to_sending_list(Message message):
-        self.sending_list.append(message)
-
-    def remove_message(message):
-        ack = message.get_ack_number()
-        id_num = message.get_id()
-        self.ack_array[ack] = False
-        self.ack_array_next_free_cell.append(ack)
-        message.delete()
-        pass
+  def remove_message(message):
+    ack = message.get_ack_number()
+    id_num = message.get_id()
+    self.ack_array[ack] = False
+    self.ack_array_next_free_cell.append(ack)
+    message.delete()
+    pass
