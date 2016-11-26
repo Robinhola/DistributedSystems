5946b6779d5c438f7c650591885354689f78acc2
Robin Elrharbi--Fleury <robin.elrharbi-fleury@phelma.grenoble-inp.fr>
2016-11-26 18:10:16 +0100

    Prepared sendingthread

--- CanalPlus/ReceivingThread.py
+++ CanalPlus/ReceivingThread.py
@@ -1,40 +1,32 @@
 class ReceivingThread(threading.Thread):
-def __init__(self, int buffer_size):
+def __init__(self, connection ,buffer_size = 35):
         super(ReceivingThread, self).__init__()
-        self.messages_to_ack = []
         self.receiving_buffer = queue()
-        self.buffer_size = buffer_size
 
     def run(self):
       while True:
-        if self.buffer_is_full():
-          self.send_signal_buffer_full()
-        if self.new_message_received():
-          self.handle_new_message()
-        message = self.pop_next_message();
-        if not message.get_ack_status():
-        if message.time_since_last_try_not_short():
-                message.try_to_send()
-            self.append_to_messages_to_ack_list(message)
-        else:
-            self.remove_message(message)
+        self.receive()
+        for message in self.receiving_buffer:
+          if (self.has_ack_number(message) and self.demands_ack(message)):
+            self.send_corresponding_ack(message)
+          else:
+            self.send_new_ack(message)
 
     def send_signal_buffer_full(self):
       pass
 
-    def new_message_received(self):
-      return len(receiving_buffer) > 0
+    def receive(self):
+      socket.recvfrom_into(self.receiving_buffer)
 
-    def handle_new_message(self):
-      Message message = self.receiving_buffer.pop()
-      self.append_to_messages_to_ack_list(message) 
+    def has_ack_number(self, message):
       pass
 
-    def pop_next_message(self):
-      return messages_to_ack.pop()
+    def demands_ack(self, message):
+      pass
 
-    def append_to_messages_to_ack_list(self, message):
-      self.messages_to_ack.append(message)
+    def send_corresponding_ack(self, message):
+      pass
 
-    def remove_message(self, message):
-      message.delete()
+    def send_new_ack(self, message):
+      pass
+
