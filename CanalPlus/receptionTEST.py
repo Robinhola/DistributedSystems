750723cc23955a31111a27c9764eb3a948c73c3d
Grégoire Gomes <gregoire.gomes@ensimag.grenoble-inp.fr>
2016-11-29 20:18:30 +0100

    Changed receptiontest to listen on all IPs

--- CanalPlus/receptionTEST.py
+++ CanalPlus/receptionTEST.py
@@ -2,7 +2,7 @@ import Connection
 
 UDP_IP = "127.0.0.1"
 
-connect = Connection.Connection()
+connect = Connection.Connection("0.0.0.0")
 while True:
   data = connect.receive()
   if len(data) > 0:
